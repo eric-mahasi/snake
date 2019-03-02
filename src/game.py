52560433146c38caac6b70be1e97953c68717019
eric <mahasieric@gmail.com>
2019-03-02 18:04:42 +0300

    Implemented movement of the snake with response to the arrow keys.
    Signed-off-by: eric <mahasieric@gmail.com>

--- src/game.py
+++ src/game.py
@@ -12,12 +12,30 @@ class Game(object):
 
     def loop(self):
         clock = pygame.time.Clock()
-
+        x_change = 0
+        y_change = 0
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -self.config.snake_speed
+                    y_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x_change = self.config.snake_speed
+                    y_change = 0
+                elif event.key == pygame.K_DOWN:
+                    x_change = 0
+                    y_change = self.config.snake_speed
+                elif event.key == pygame.K_UP:
+                    x_change = 0
+                    y_change = -self.config.snake_speed
+
+            self.display.fill(self.config.black)
+            self.mamba.move(x_change, y_change)
+            self.mamba.draw()
             pygame.display.update()
             clock.tick(self.config.fps)
